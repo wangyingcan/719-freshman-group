3769abd75908d6161ff2aa77f4867c882fb33fde
王颖璨 <76271052+wangyingcan@users.noreply.github.com>
2024-01-13 13:40:52 +0800

    Add files via upload
    知识图谱复现后端项目初始化

---
+++ knowledgeGraph-backend/src/test/java/com/example/knowledgeGraph/knowledgeGraph/KnowledgeGraphBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.knowledgeGraph.knowledgeGraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeGraphBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
