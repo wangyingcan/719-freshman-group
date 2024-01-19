65412ad936d9c11559ff975e98388c4fc1054f79
王颖璨 <2548339747@qq.com>
2024-01-19 17:30:08 +0800

    1.19 搭建完成

--- knowledgeGraph-backend/src/main/java/com/example/knowledgeGraph/knowledgeGraph/app/restful/TestController.java
+++ knowledgeGraph-backend/src/main/java/com/example/knowledgeGraph/knowledgeGraph/app/restful/TestController.java
@@ -19,23 +19,32 @@ public class TestController {
     // 1.get方法的测试案例:直接返回
     @GetMapping("/get")
     public CommonResult<TestVO> get() {
+        // 1.1 初始化VO对象，最后需要作为响应体中data属性进行返回
         TestVO testVO = new TestVO();
+        // 1.2 跳过service、dao层，直接设置测试属性
         testVO.setUserId("123");
         testVO.setUserName("wangyingcan");
-        return new CommonResult<TestVO>().makeSuccess().addData(testVO);
+        // 1.3 封装响应体
+        return new CommonResult<TestVO>().makeSuccess().addData(testVO).addMessage("get方法测试成功");
     }
 
     // 2.post方法的测试案例
     @PostMapping("/post")
+    // 2.1 @RequestBody注解表示从请求体中获取query对象
     public CommonResult<TestVO> post(@RequestBody TestPostQuery testPostQuery) {
+        // 2.2 判断是否获取到了请求体的Query对象！！！
         if (testPostQuery != null) {
+            // 2.3 查询数据库，判断是否存在该用户
             if (testPostQuery.getUserId().equals("123")) {
                 TestVO result = new TestVO("wangyingcan", "123");
+                // 2.4 封装响应体
                 return new CommonResult<TestVO>().makeSuccess().addMessage("post方法成功").addData(result);
             } else {
+                // 2.5 封装失败响应体
                 return new CommonResult<TestVO>().makeFail().addMessage("post方法失败");
             }
         }
-        return new CommonResult<TestVO>().makeFail().addMessage("post方法失败,没有参数!!!");
+        // 2.6 封装失败响应体
+        return new CommonResult<TestVO>().makeFail().addMessage("post方法失败,没有成功获取参数!!!");
     }
 }
